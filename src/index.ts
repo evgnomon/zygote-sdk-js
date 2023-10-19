f5dc0beb35a3b8d6ff2201a20227303815125c7a
Hamed Ghasemzadeh <hamed.ghasemzadeh@clusterlean.com>
2023-10-19 21:27:14 +0000

    feat: DB local connection

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import mysql from "mysql";
+
 export function sayHello() {
   console.log("hi");
 }
@@ -8,4 +10,44 @@ export function sayHello() {
  */
 export function sayGoodbye() {
   console.log("goodbye");
+}
+
+const gwHostname = "127.0.0.2";
+
+// DOCKER_NETWORK_NAME=mynet
+// DOCKER_GATEWAY_HOST=127.0.0.1
+// DB_SHARD_1_EXTERNAL_PORT= 3306
+// DB_SHARD_2_EXTERNAL_PORT= 3307
+// REDIS_PORT=6373
+// REDIS_SHARD_1_INTERNAL_HOST=clapp-mem-shard-1
+// REDIS_SHARD_2_INTERNAL_HOST=clapp-mem-shard-2
+// REDIS_SHARD_3_INTERNAL_HOST=clapp-mem-shard-3
+// PORT=3000
+// HTTP_HOST=localhost
+//
+
+function resolveEndpoint() {
+  let portNum = 3306;
+  if (process.env.DB_SHARD_1_INTERNAL_PORT) {
+    portNum = parseInt(process.env.DB_SHARD_1_INTERNAL_PORT);
+  }
+  return {
+    host: process.env.DB_SHARD_1_INTERNAL_HOST || gwHostname,
+    port: portNum,
+  };
+}
+
+type NumberCallback = (err: unknown, n: mysql.Connection) => unknown;
+
+export function connect(): Promise<mysql.Connection> {
+  return new Promise((resolve, reject) => {
+    const connection = mysql.createConnection({
+      ...resolveEndpoint(),
+      user: "test_1",
+      password: "password",
+      database: "myproject_1",
+    });
+    connection.connect();
+    resolve(connection);
+  });
 }
--- test/sample.spec.js
+++ test/sample.spec.js
@@ -1,10 +1,24 @@
 import { assert } from "chai";
 
+import { connect } from "../src/index";
+
 describe("another case", function () {
   const expected = { hello: "world" };
-  let actual;
-  before(function () {
-    actual = { hello: "world" };
+  let actual = { hello: "world" };
+  let conn;
+  before(async function () {
+    conn = await connect();
+    console.log("connected");
+    const results = await new Promise((resolve, reject) => {
+      conn.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
+        if (error) reject(error);
+        resolve(results);
+      });
+    });
+    console.log("The solution is: ", results[0].solution);
+  });
+  after(async function () {
+    await conn.end();
   });
   it("returns desired response", function () {
     assert.deepStrictEqual(actual, expected);
