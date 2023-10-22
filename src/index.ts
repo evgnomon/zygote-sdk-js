c13b3685c46bbf0c27b7314e505fd3f6b1cd360b
Hamed Ghasemzadeh <hamed.ghasemzadeh@clusterlean.com>
2023-10-22 17:29:54 +0200

    feat: DB local connection (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import mysql from "mysql";
+
 export function sayHello() {
   console.log("hi");
 }
@@ -8,4 +10,76 @@ export function sayHello() {
  */
 export function sayGoodbye() {
   console.log("goodbye");
+}
+
+// DOCKER_NETWORK_NAME=mynet
+// DOCKER_GATEWAY_HOST=127.0.0.1
+// DB_SHARD_1_EXTERNAL_PORT= 3306
+// DB_SHARD_2_EXTERNAL_PORT= 3307
+// REDIS_PORT=6373
+// REDIS_SHARD_1_INTERNAL_HOST=clapp-mem-shard-1
+// REDIS_SHARD_2_INTERNAL_HOST=clapp-mem-shard-2
+// REDIS_SHARD_3_INTERNAL_HOST=clapp-mem-shard-3
+// PORT=3000
+// HTTP_HOST=localhost
+//
+
+function resolveEndpoint(shard: number) {
+  let portNum = 3306 - 1 + shard;
+  if (process.env[`DB_SHARD_${shard}_INTERNAL_PORT`]) {
+    portNum = parseInt(process.env[`DB_SHARD_${shard}_INTERNAL_PORT`] || "0");
+  }
+  return {
+    host: process.env[`DB_SHARD_${shard}_INTERNAL_HOST`] || "127.0.0.1",
+    port: portNum,
+  };
+}
+
+function createPool(shard: number): Promise<mysql.Pool> {
+  return new Promise((resolve, reject) => {
+    try {
+      const pool = mysql.createPool({
+        ...resolveEndpoint(shard),
+        user: `test_${shard}`,
+        password: "password",
+        database: `myproject_${shard}`,
+      });
+      resolve(pool);
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+export class DB {
+  pool: Promise<mysql.Pool>;
+  constructor(shardNum = 1) {
+    this.pool = createPool(shardNum);
+  }
+
+  async query(sql: string): Promise<[unknown, mysql.FieldInfo[] | undefined]> {
+    const pool = await this.pool;
+    return new Promise((resolve, reject) => {
+      pool.query(sql, (err, results, fields) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve([results, fields]);
+      });
+    });
+  }
+
+  async close(): Promise<void> {
+    const pool = await this.pool;
+    return new Promise((resolve) => {
+      pool.end((err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        resolve(undefined);
+      });
+    });
+  }
 }
--- test/sample.spec.js
+++ test/sample.spec.js
@@ -1,12 +1,242 @@
 import { assert } from "chai";
 
-describe("another case", function () {
-  const expected = { hello: "world" };
-  let actual;
-  before(function () {
-    actual = { hello: "world" };
+import { DB } from "../src/index";
+
+describe("DB.query", function () {
+  let db;
+  before(async function () {
+    db = new DB(1);
   });
-  it("returns desired response", function () {
-    assert.deepStrictEqual(actual, expected);
+  after(async function () {
+    await db.close();
   });
+
+  const testParams = [
+    {
+      name: "select users",
+      query: "select * from users where username IN ('john_doe', 'sam_smith');",
+      results: [
+        {
+          id: 1,
+          username: "john_doe",
+          password: "hashed_password1",
+          email_address: "john.doe@email.com",
+          phone_number: "+1234567890",
+        },
+        {
+          id: 3,
+          username: "sam_smith",
+          password: "hashed_password3",
+          email_address: "sam.smith@email.com",
+          phone_number: "+1122334455",
+        },
+      ],
+      fields: [
+        {
+          catalog: "def",
+          db: "myproject_1",
+          table: "users",
+          orgTable: "users",
+          name: "id",
+          orgName: "id",
+          charsetNr: 63,
+          length: 11,
+          type: 3,
+          flags: 16899,
+          decimals: 0,
+          zeroFill: false,
+          protocol41: true,
+        },
+        {
+          catalog: "def",
+          db: "myproject_1",
+          table: "users",
+          orgTable: "users",
+          name: "username",
+          orgName: "username",
+          charsetNr: 33,
+          length: 765,
+          type: 253,
+          flags: 20485,
+          decimals: 0,
+          zeroFill: false,
+          protocol41: true,
+        },
+        {
+          catalog: "def",
+          db: "myproject_1",
+          table: "users",
+          orgTable: "users",
+          name: "password",
+          orgName: "password",
+          charsetNr: 33,
+          length: 765,
+          type: 253,
+          flags: 4097,
+          decimals: 0,
+          zeroFill: false,
+          protocol41: true,
+        },
+        {
+          catalog: "def",
+          db: "myproject_1",
+          table: "users",
+          orgTable: "users",
+          name: "email_address",
+          orgName: "email_address",
+          charsetNr: 33,
+          length: 765,
+          type: 253,
+          flags: 20485,
+          decimals: 0,
+          zeroFill: false,
+          protocol41: true,
+        },
+        {
+          catalog: "def",
+          db: "myproject_1",
+          table: "users",
+          orgTable: "users",
+          name: "phone_number",
+          orgName: "phone_number",
+          charsetNr: 33,
+          length: 60,
+          type: 253,
+          flags: 20485,
+          decimals: 0,
+          zeroFill: false,
+          protocol41: true,
+        },
+      ],
+    },
+    {
+      query:
+        "select id, title, author, publication_year, description from books;",
+      results: [
+        {
+          id: 1,
+          title: "1984",
+          author: "George Orwell",
+          publication_year: 1949,
+          description: "Dystopian novel",
+        },
+        {
+          id: 2,
+          title: "To Kill a Mockingbird",
+          author: "Harper Lee",
+          publication_year: null,
+          description: null,
+        },
+        {
+          id: 3,
+          title: "The Great Gatsby",
+          author: "F. Scott Fitzgerald",
+          publication_year: 1925,
+          description: null,
+        },
+        {
+          id: 4,
+          title: "Moby-Dick",
+          author: "Herman Melville",
+          publication_year: null,
+          description: null,
+        },
+      ],
+      fields: [
+        {
+          catalog: "def",
+          db: "myproject_1",
+          table: "books",
+          orgTable: "books",
+          name: "id",
+          orgName: "id",
+          charsetNr: 63,
+          length: 11,
+          type: 3,
+          flags: 16899,
+          decimals: 0,
+          zeroFill: false,
+          protocol41: true,
+        },
+        {
+          catalog: "def",
+          db: "myproject_1",
+          table: "books",
+          orgTable: "books",
+          name: "title",
+          orgName: "title",
+          charsetNr: 33,
+          length: 765,
+          type: 253,
+          flags: 4097,
+          decimals: 0,
+          zeroFill: false,
+          protocol41: true,
+        },
+        {
+          catalog: "def",
+          db: "myproject_1",
+          table: "books",
+          orgTable: "books",
+          name: "author",
+          orgName: "author",
+          charsetNr: 33,
+          length: 765,
+          type: 253,
+          flags: 4097,
+          decimals: 0,
+          zeroFill: false,
+          protocol41: true,
+        },
+        {
+          catalog: "def",
+          db: "myproject_1",
+          table: "books",
+          orgTable: "books",
+          name: "publication_year",
+          orgName: "publication_year",
+          charsetNr: 63,
+          length: 11,
+          type: 3,
+          flags: 0,
+          decimals: 0,
+          zeroFill: false,
+          protocol41: true,
+        },
+        {
+          catalog: "def",
+          db: "myproject_1",
+          table: "books",
+          orgTable: "books",
+          name: "description",
+          orgName: "description",
+          charsetNr: 33,
+          length: 196605,
+          type: 252,
+          flags: 16,
+          decimals: 0,
+          zeroFill: false,
+          protocol41: true,
+        },
+      ],
+    },
+  ];
+
+  for (const { query, results, fields } of testParams) {
+    describe(`with ${query}`, function () {
+      let actualResults, actualFields;
+      before(async function () {
+        [actualResults, actualFields] = await db.query(query);
+      });
+      it("returns desired results", function () {
+        assert.deepStrictEqual(actualResults, results);
+      });
+      it("returns desired fields", function () {
+        assert.deepStrictEqual(
+          JSON.parse(JSON.stringify(actualFields)),
+          fields
+        );
+      });
+    });
+  }
 });
