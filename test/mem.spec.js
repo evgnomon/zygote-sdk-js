30e621d21a7c15845d531bad849a1586c4e87768
Hamed Ghasemzadeh <hamed.ghasemzadeh@clusterlean.com>
2024-10-15 11:03:16 +0200

    Publish node module (#5)

--- test/mem.spec.js
+++ test/mem.spec.js
@@ -7,24 +7,31 @@ describe("MEM", function () {
   const value = {
     hello: "world",
   };
+
   before(async function () {
     mem = await new MEM().connect();
   });
+
   after(async function () {
     await mem.quit();
   });
+
   describe("set and get test-key", function () {
     const testKey = "test-key";
+
     before(async function () {
       await mem.set(testKey, JSON.stringify(value));
     });
+
     it("returns the written test key", async function () {
       const userSession = await mem.get(testKey);
       assert.deepStrictEqual(userSession, JSON.stringify(value));
     });
+
     it("del key", async function () {
       await mem.del(testKey);
     });
+
     it("doesn't return the deleted test key", async function () {
       const userSession = await mem.get(testKey);
       assert.notOk(userSession);
